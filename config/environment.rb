5b0415fd1120862380423c1c076d13f753999f44
francoisoudot <francois.oudot@gmail.com>
2015-08-11 19:27:42 -0700

    mail

--- config/environment.rb
+++ config/environment.rb
@@ -5,18 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
   ActionMailer::Base.smtp_settings = {
-  # :user_name => 'francois.oudot',
-  # :password => 'fro261813',
-  # :domain => 'http://whatever.com/',
-  # :address => 'smtp.sendgrid.net',
-  # :port => 587,
-  # :authentication => :plain,
-  # :enable_starttls_auto => true
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'baci.lindsaar.net',
-  :user_name            => 'momganizer@gmail.com',
-  :password             => 'fromomganizer',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  
+  :user_name => 'francois.oudot',
+  :password => 'fro261813',
+  :domain => 'http://whatever.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+
 }
